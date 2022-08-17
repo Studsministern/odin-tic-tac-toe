ff3da5601cb4834f5a0e6decfbcc2215f508b0b6
Eric Weidow <eric@weidow.se>
2022-08-17 10:03:25 +0200

    Add displayController module
    Currently only has variables and functions for player 1, player 2 and
    resetting the gameBoard.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
+const displayController = (() => {
+    const _player1 = Player('X');
+    const _player2 = Player('O');
+
+    const getPlayer1 = () => _player1;
+    const getPlayer2 = () => _player2;
+
+    const reset = () => {
+        for(let i = 0; i <= 8; i++) {
+            gameBoard.clearField(i);
+        }
+    }
+
+    return {
+        getPlayer1,
+        getPlayer2,
+        reset
+    }
+})();
+
 const gameBoard = ((htmlBoard) => {
     let _board = new Array(9);
 
