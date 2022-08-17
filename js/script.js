1a230b9f8792b6aae962cad360162dedae64eb68
Eric Weidow <eric@weidow.se>
2022-08-17 10:05:29 +0200

    Add private functions to displayController
    Functions for checking if a player has won on a row, a column or a
    diagonal. The functions return null if noone has won, and returns the
    player that has won otherwise:
    - _checkRow needs the row index from 0 to 2.
    - _checkColumn needs the column index from 0 to 2.
    - _checkDiagonal needs both the row and column index, from 0 to 2.
    
    In these functions, row 0 corresponds to the top row and column 0
    corresponds to the left column. When a new sign is added to the
    gameBoard at position (row, column), only the relevant row, column and
    diagonal has to be checked for a win using these functions.

--- js/script.js
+++ js/script.js
@@ -5,6 +5,87 @@ const displayController = (() => {
     const getPlayer1 = () => _player1;
     const getPlayer2 = () => _player2;
 
+    const _checkRow = (row) => {
+        let player1Points = 0;
+        let player2Points = 0;
+
+        for(let i = 3 * row; i < 3 * (row + 1); i++) {
+            switch(gameBoard.getField(i)) {
+                case _player1.getSign():
+                    player1Points++;
+                    break;
+                case _player2.getSign():
+                    player2Points++;
+                    break;
+                default:
+                    return null;
+            }
+        }
+
+        if(player1Points === 3) return _player1;
+        if(player2Points === 3) return _player2;
+    }
+
+    const _checkColumn = (col) => {
+        let player1Points = 0;
+        let player2Points = 0;
+
+        for(let i = col; i <= 8; i += 3) {
+            switch(gameBoard.getField(i)) {
+                case _player1.getSign():
+                    player1Points++;
+                    break;
+                case _player2.getSign():
+                    player2Points++;
+                    break;
+                default:
+                    return null;
+            }
+        }
+    
+        if(player1Points === 3) return _player1;
+        if(player2Points === 3) return _player2;
+    }
+
+    const _checkDiagonal = (row, col) => {
+        if((row === 1 && col !== 1) || (row !== 1 && col === 1)) { // Does not have to check middle of each side. Center (row === 1 and col === 1)
+            return null;                                           // is checked in the column from top left to bottom right
+        }
+        let player1Points = 0;
+        let player2Points = 0;
+
+        if(row === col) { // Handles column from top left to bottom right
+            for(let i = 0; i <= 2; i++) {
+                switch(gameBoard.getField(4 * i)) { // 4 * i for index 0, 4 and 8 (top left, middle, bottom right)
+                    case _player1.getSign():
+                        player1Points++;
+                        break;
+                    case _player2.getSign():
+                        player2Points++;
+                        break;
+                    default:
+                        return null;
+                }
+            }
+        } else { // Handles column from top right to bottom left
+            for(let i = 2; i <= 6; i += 2) {
+                switch(gameBoard.getField(i)) { // Index 2, 4 and 6 (top right, middle, bottom left)
+                    case _player1.getSign():
+                        player1Points++;
+                        break;
+                    case _player2.getSign():
+                        player2Points++;
+                        break;
+                    default:
+                        return null;
+                }
+            }
+        }
+
+        if(player1Points === 3) return _player1;
+        if(player2Points === 3) return _player2;
+    }
+    
     const reset = () => {
         for(let i = 0; i <= 8; i++) {
             gameBoard.clearField(i);
